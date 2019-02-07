17bd586ab63417e514bbb25f81731c761a68663b
matheus <matheusmacielmelchiades@hotmail.com>
2019-02-07 16:16:25 -0200

    fix cors

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://twitter-server-clone.herokuapp.com'
+    baseURL: 'https://twitter-server-clone.herokuapp.com'
 });
 
 export default api;
